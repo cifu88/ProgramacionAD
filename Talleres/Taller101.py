25705b12dfb82781528c79aae8b8c1f3610c284c
cifu88 <scifuentes84796@umanizales.edu.co>
2022-08-03 19:44:25 -0500

    Taller101

--- Talleres/Taller101.py
+++ Talleres/Taller101.py
@@ -1,4 +1,32 @@
-print("Hello world")
-# Prueba de gitHub
-a = 2
-print(a)
+"""Exercise: Your First Program
+Generations of programmers have started their coding careers by simply printing "Hello, world!". 
+You will be following in their footsteps."""
+from re import X
+
+
+print('Hello, world!')
+
+"""Now, let's enhance your code with a comment. 
+Print out the phrase: Hello, world! and comment it with the phrase Print the traditional hello world all in one line of code."""
+print("Hello, world!") #Print the traditional hello world
+
+"""Use the type() function to check the object type of 12.0."""
+type(12.0)
+
+"""What is the data type of the result of: 6 / 2?"""
+type(6/2)
+
+"""What is the type of the result of: 6 // 2? (Note the double slash //.)"""
+type(6//2)
+
+"""What is the value of x where x = 3 + 2 * 2"""
+x = 3 + 2 * 2
+print(x)
+
+"""What is the value of y where y = (3 + 2) * 2?"""
+y = (3 + 2) * 2
+print(y)
+
+"""What is the value of z where z = x + y?"""
+z = x + y
+print(z)
