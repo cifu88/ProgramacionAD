752c98cb58e78f874a4b3291704795252670e35d
cifu88 <scifuentes84796@umanizales.edu.co>
2022-08-03 20:01:43 -0500

    Taller1

--- Talleres/Taller101.py
+++ Talleres/Taller101.py
@@ -1,3 +1,4 @@
+print("TALLER 1.1")
 """Exercise: Your First Program
 Generations of programmers have started their coding careers by simply printing "Hello, world!". 
 You will be following in their footsteps."""
@@ -30,3 +31,41 @@ print(y)
 """What is the value of z where z = x + y?"""
 z = x + y
 print(z)
+
+print("TALLER 1.2")
+
+"""What is the value of the variable a after the following code is executed?"""
+a = '1'
+print(a)
+
+"""What is the value of the variable b after the following code is executed?"""
+b = '2'
+print(b)
+
+"""What is the value of the variable c after the following code is executed?"""
+c = a + b
+print(c)
+
+"""Consider the variable d use slicing to print out the first three elements:"""
+d = "ABCDEFG"
+print(d[0:3])
+
+"""Use a stride value of 2 to print out every second character of the string e"""
+e = 'clocrkr1e1c1t'
+print(e[0::2])
+
+"""Print out a backslash:"""
+print("\\ ")
+
+"""Convert the variable f to uppercase:"""
+f = "You are wrong"
+print(f.upper())
+
+"""Consider the variable g, and find the first index of the sub-string snow"""
+g = "Mary had a little lamb Little lamb, little lamb Mary had a little lamb \
+Its fleece was white as snow And everywhere that Mary went Mary went, Mary went \
+Everywhere that Mary went The lamb was sure to go"
+print(g.find('snow'))
+
+"""In the variable g, replace the sub-string Mary with Bob:"""
+print(g.replace('Mary', 'Bob'))
